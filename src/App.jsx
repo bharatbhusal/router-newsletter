ceb4a84a6596ccb0ff211fe290c6e0d3a6865565
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-02-05 12:21:54 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import "./App.css";
-import MonthlyView from './components/MonthlyView';
-import january from "./news/2024/january.json";
 import NavBar from './components/NavBar';
 import { Outlet } from 'react-router-dom';
-import NewsTextFileGenerator from './components/DownloadHandler';
 import DownloadHandler from './components/DownloadHandler';
 
 
 const App = () => {
-
-
     return (
-        <div style={{ gap: "0.4rem" }}>
-            <div className='container'>
-                <Outlet />
-                <DownloadHandler />
-            </div>
+        // <div style={{ gap: "0.4rem" }}>
+        <div className='container'>
+            <DownloadHandler />
+            <Outlet />
             <NavBar />
+            {/* </div> */}
         </div>
     );
 };
