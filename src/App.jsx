38c02ecd95e045c4ffd934c1ab9fcc8b20ae94ba
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-02-10 01:39:02 +0530

    adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import "./App.css";
 import NavBar from './components/NavBar';
 import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
 
 const App = () => {
 
     return (
-        <div className='container flex space-around'>
+        <div className='container flex flex-column space-between'>
             <Outlet />
             <NavBar />
+            <Footer />
         </div>
     );
 };
