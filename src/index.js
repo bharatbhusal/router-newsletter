56b5220793f94994678e282660cdb11cf3bdec04
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-04-08 22:02:01 +0530

    april default month

--- src/index.js
+++ src/index.js
@@ -1,57 +1,85 @@
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import MonthlyView from "./components/MonthlyView";
 import NotFoundPage from "./components/404Error";
 
-
 // Array of month names
 const months = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
 ];
 
 // Dynamically import each month's data
-const importMonthData = async (month) => (await import(`./news/2024/${month.toLowerCase()}.json`)).default;
+const importMonthData = async (month) =>
+	(await import(`./news/2024/${month.toLowerCase()}.json`))
+		.default;
 
 // Create an object to store the imported month data
-const monthDataPromises = months.map(month => importMonthData(month));
+const monthDataPromises = months.map((month) =>
+	importMonthData(month)
+);
 
 const initializeApp = async () => {
-    const monthData = await Promise.all(monthDataPromises);
+	const monthData = await Promise.all(monthDataPromises);
 
-    // Getting the DOM element with the id "root"
-    const divContainer = document.getElementById("root");
+	// Getting the DOM element with the id "root"
+	const divContainer = document.getElementById("root");
 
-    // Creating a root for the React application using createRoot and associating it with the divContainer  
-    const root = createRoot(divContainer);
+	// Creating a root for the React application using createRoot and associating it with the divContainer
+	const root = createRoot(divContainer);
 
-    const router = createBrowserRouter([
-        {
-            path: "",
-            element: <App />,
-            errorElement: <NotFoundPage />,
-            children: [
-                {
-                    errorElement: <NotFoundPage />,
-                    children: [
-                        {
-                            index: true,
-                            element: <MonthlyView year="2024" month="March" newsData={monthData[2]} />
-                        },
-                        ...months.map((month, index) => ({
-                            path: month.toLowerCase(),
-                            element: <MonthlyView year="2024" month={month} newsData={monthData[index]} />,
-                        })),
-                    ]
-                },
-            ],
-        },
-    ]);
+	const router = createBrowserRouter([
+		{
+			path: "",
+			element: <App />,
+			errorElement: <NotFoundPage />,
+			children: [
+				{
+					errorElement: <NotFoundPage />,
+					children: [
+						{
+							index: true,
+							element: (
+								<MonthlyView
+									year="2024"
+									month="March"
+									newsData={monthData[3]}
+								/>
+							),
+						},
+						...months.map((month, index) => ({
+							path: month.toLowerCase(),
+							element: (
+								<MonthlyView
+									year="2024"
+									month={month}
+									newsData={monthData[index]}
+								/>
+							),
+						})),
+					],
+				},
+			],
+		},
+	]);
 
-    // Rendering the main App component within the root
-    root.render(<RouterProvider router={router} />);
+	// Rendering the main App component within the root
+	root.render(<RouterProvider router={router} />);
 };
 
 // Initialize the app
