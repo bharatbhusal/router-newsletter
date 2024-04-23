b6a76d881bda228498852cdb02242dea7d6b2639
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-04-24 00:14:37 +0530

    optimize api

--- news/src/api.js
+++ news/src/api.js
@@ -2,6 +2,17 @@ import env from "./utils/validateEnv";
 import localForage from "localforage";
 // Read
 const getOneMonthNews = async (year, month) => {
+	const today = new Date();
+	const currentMonth = today.getMonth() + 1;
+	if (currentMonth === month) {
+		// Retrieve the data from the endpoint
+		const response = await fetch(
+			`${env.REACT_APP_SERVER_URL}/${year}/${month}`
+		);
+		const data = await response.json();
+		return data;
+	}
+
 	// Check if the news exists in localForage
 	const cachedData = await localForage.getItem(
 		`${year}-${month}`
